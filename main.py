24c87daa4caf23c3575669a3dba0cd6ecb743fdf
Grant <grant.brosovich@ufl.edu>
2023-07-19 15:22:03 -0400

    This is my new and improved encoder

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def encoder(code):
         num_code.append(int(v))
     for i in range(len(num_code)):
         num_code[i] += 3
+        if num_code[i] >= 10:
+            num_code[i] = num_code[i] % 10
         num_code[i] = str(num_code[i])
     encoded_pass = ''.join(num_code)
     return encoded_pass
@@ -17,6 +19,7 @@ def menu():
     print("1. Encode my password")
     print("2. Decode my password")
     print("3. Exit")
+    print("Select an option to perform.")
     return
 
 if __name__ == "__main__":
@@ -25,9 +28,17 @@ if __name__ == "__main__":
         menu()
         choice = int(input())
         if choice == 1:
-            print("Enter the password to decode:", end = '')
-            new_password = encoder(str(input()))
-            print(new_password)
+            print("Enter an 8-Digit password to encode:", end = '')
+            try:
+               new_password = int(input())
+            except ValueError:
+                print('The 8-digit password contains an incorrect character. Please try again')
+            if len(str(new_password)) != 8:
+                print('That is an an valid password! Please type and 8-Digit password')
+            elif len(str(new_password)) == 8:
+                new_password = encoder(str(new_password))
+                print(f'The encoded password is: {new_password}')
+
 
         if choice == 2:
             '''place decoder function here'''
