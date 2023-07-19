dd7daf129a4e94be85b19815281a88f5a2ecf3bc
t0re0 <vafernandez191@gmail.com>
2023-07-19 09:37:42 -0400

    added comments, decoder function, and edited menu. I found error in the encoder fn that may need correction

--- main.py
+++ main.py
@@ -12,6 +12,19 @@ def encoder(code):
     encoded_pass = ''.join(num_code)
     return encoded_pass
 
+
+# hello edit!! hey grant, make sure to comment on code :)
+
+
+def decode_password(encoded_password):
+    decoded_password = ""  # initialize empty string
+    for digit in encoded_password:  # iterate over each digit with for loop
+        # Convert the digit to an integer, subtract 3, and wrap around if it goes below 0
+        decoded_digit = str((int(digit) - 3) % 10)
+        decoded_password += decoded_digit
+    return decoded_password
+
+
 def menu():
     print("Please select an option")
     print("1. Encode my password")
@@ -19,21 +32,27 @@ def menu():
     print("3. Exit")
     return
 
+
 if __name__ == "__main__":
     coding = True
-    while coding == True:
+    while True:
         menu()
         choice = int(input())
         if choice == 1:
-            print("Enter the password to decode:", end = '')
+            print("Enter the password to encode:", end='')
             new_password = encoder(str(input()))
             print(new_password)
 
-        if choice == 2:
-            '''place decoder function here'''
+        elif choice == 2:  # inverse of 1, intakes 8 dig, converts to original password.
+            encoded_password = input("Enter the password to encode: ")
+            if len(encoded_password) == 8:
+                decoded_password = decode_password(encoded_password)
+                print("Decoded password:", decoded_password)
+            else:  # if not 8 dig, print error
+                print("Invalid encoded password. Please enter an 8-digit encoded password.")
 
-        if choice == 3:
-            print("Dueces!")
+        elif choice == 3:
+            print("Deuces!")
             break
-        if choice < 0 or choice > 3:
+        elif choice < 0 or choice > 3:
             print("That is not a valid option!")
